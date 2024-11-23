4b940ef737b0b83a6c0a42187b9da37811074134
Othmane Bouizi <obouizi@e1r5p1.1337.ma>
2024-11-23 12:55:55 +0100

    setup the project

---
+++ library_files/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouizi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 10:14:37 by obouizi           #+#    #+#             */
+/*   Updated: 2024/11/06 12:12:43 by obouizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if (size != 0 && count > (SIZE_MAX / size))
+		return (NULL);
+	ptr = malloc(size * count);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
